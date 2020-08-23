7646555544dbf02565c85e7425f90c3d42384ff8
nimitkansagra <18bce360@nirmauni.ac.in>
2020-08-23 14:32:08 +0530

    DNS Cheker

--- api/dns/getdns.php
+++ api/dns/getdns.php
@@ -2,6 +2,6 @@
 	if(isset($_GET['domain'])&&isset($_GET['record'])){
 		$domain = trim($_GET['domain']);
 		$record = trim($_GET['record']);
-		echo shell_exec("python3 dnss.py ".$domain." ".$record);
+		echo shell_exec("python dnss.py ".$domain." ".$record);
 	}
 ?>
--- api/geoloc/getlocation.php
+++ api/geoloc/getlocation.php
@@ -1,6 +1,6 @@
 <?php
 	if(isset($_GET['ip'])){
 		$ip = trim($_GET['ip']);
-		echo shell_exec("python3 geoloc.py '".$ip."'");
+		echo shell_exec("python geoloc.py '".$ip."'");
 	}
 ?>
