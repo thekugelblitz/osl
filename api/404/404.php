f1f725ccc8b2f2a141b1b142b5d3a302d51868df
nimitkansagra <18bce360@nirmauni.ac.in>
2020-09-04 16:52:41 +0530

    V.0.2

---
+++ api/404/404.php
@@ -0,0 +1,3 @@
+<?php
+	echo shell_exec('python 404.py');
+?>
---
+++ api/404/404.py
@@ -0,0 +1,4 @@
+from usp.tree import sitemap_tree_for_homepage
+
+tree = sitemap_tree_for_homepage('https://hostingspell.com/')
+print(tree)
